67a1a1840b5919d9ad5b1b4d66ee58dcb45d120d
a8anassis <a8anassis@gmail.com>
2025-07-10 18:38:32 +0300

    feat: add number exercises

--- number-exercises.js
+++ number-exercises.js
@@ -7,3 +7,28 @@ const formatCurrency = amount => {
 
 console.log(formatCurrency(12.3456))        // 12.34
 console.log(formatCurrency(NaN))
+
+// Safe Division
+const safeDivide = (a, b) => {
+    a = a ?? 0;     // nullish coalescing
+    b = b ?? 1;
+
+    return (b === 0) ? Infinity : a / b;
+}
+
+console.log(safeDivide(10, 2))
+console.log(safeDivide(10, 0))
+console.log(safeDivide(null, 2))
+
+// Random Hex Color Generator
+const getRandomHexColor = () => {
+    const color = Math.floor(Math.random() * (0xFFFFFF + 1)).toString(16).padStart(6, '0')
+    return `#${color}`
+}
+
+console.log(getRandomHexColor())
+console.log(getRandomHexColor())
+console.log(getRandomHexColor())
+console.log(getRandomHexColor())
+console.log(getRandomHexColor())
+
